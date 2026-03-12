1073e1f2bf6d25c8078ab5481112d4d4107559d9
Sarah.git <59260@students.riphah.edu.pk>
2026-03-12 13:14:01 +0500

    Added VariablesDemo program

---
+++ src/VariablesDemo.java
@@ -0,0 +1,18 @@
+public class VariablesDemo {
+
+    public static void main(String[] args) {
+
+        int age = 20;
+        double salary = 20000.5;
+        char grade = 'A';
+        boolean active = true;
+        String name = "Sara";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Active: " + active);
+
+    }
+}
